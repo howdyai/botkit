d55dc8343e399df4cad694c99dc20d43f687cfaf
Ben Brown <ben@xoxco.com>
2015-11-23 16:49:06 -0600

    add some examples of using the simple storage class

--- hello_world_rtm.js
+++ hello_world_rtm.js
@@ -1,18 +1,53 @@
 var Bot = require('./Slackbot.js');
 
-var bot = Bot();
-
-bot.init();
+var bot = Bot({
+ path: './db/',
+});
 
 bot.startRTM({
   team: {
     token: process.env.token
   }
+},function(err) {
+
+  if (err) {
+    throw new Error(err);
+  }
+
 });
 
 bot.hears(['hello'],'direct_message,direct_mention',function(message) {
   bot.reply(message,'Hello!');
 });
+
+
+bot.hears(['my name is (.*)'],'direct_message,direct_mention',function(message) {
+  var matches = message.text.match(/my name is (.*)/i);
+  var name = matches[1];
+  bot.storage.users.get(message.user,function(err,user) {
+    if (!user) {
+      user = {
+        id: message.user,
+      }
+    }
+    user.name = name;
+    bot.storage.users.save(user,function(err,id) {
+      bot.reply(message,'Got it. I will call you ' + user.name + ' from now on.');
+    })
+  })
+});
+
+
+bot.hears(['what is my name'],'direct_message,direct_mention',function(message) {
+  bot.storage.users.get(message.user,function(err,user) {
+    if (user && user.name) {
+      bot.reply(message,'Your name is ' + user.name);
+    } else {
+      bot.reply(message,'I don\'t know your name yet');
+    }
+  })
+});
+
 
 bot.hears(['question','ask'],'direct_message,direct_mention',function(message) {
   bot.startConversation(message,function(convo) {
