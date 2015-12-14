a3377602dea1a841d65ddb05cd86abc61902beff
Ben Brown <ben@xoxco.com>
2015-12-13 21:00:52 -0600

    add license clean up a few old test files move docs to root

--- bot.js
+++ bot.js
@@ -79,6 +79,18 @@ var bot = controller.spawn(
 
 
 controller.hears(['hello','hi'],'direct_message,direct_mention,mention',function(bot,message) {
+
+  bot.api.reactions.add({
+    timestamp: message.ts,
+    channel: message.channel,
+    name: 'robot_face',
+  },function(err,res) {
+    if (err) {
+      bot.log("Failed to add emoji reaction :(",err);
+    }
+  });
+
+
   controller.storage.users.get(message.user,function(err,user) {
     if (user && user.name) {
       bot.reply(message,"Hello " + user.name+"!!");
