b96bedc7193a998509b6976e90c83ae4d95a458b
brianleroux <b@brian.io>
2015-12-16 22:48:41 -0800

    adding tests

---
+++ test/Slack_web_api-test.js
@@ -0,0 +1,39 @@
+var test = require('tape')
+var Botkit = require('../')
+var env = require('node-env-file')
+var path = require('path')
+
+env(path.join(__dirname, '..', '.env'))
+var token = {token:process.env.TOKEN}
+
+test('sanity', t=> {
+  t.plan(4)
+  t.ok(token, '.env sets TOKEN')
+  console.log(token)
+  t.ok(Botkit, 'Botkit exists')
+  t.ok(Botkit.core, 'Botkit.core exists')
+  t.ok(Botkit.slackbot, 'Botkit.slackbot exists')
+  console.log(Botkit)
+})
+
+test('can start and then stop a bot', t=> {
+
+  var controller = Botkit.slackbot({debug:false}) 
+  var bot = controller.spawn(token).startRTM((err, bot, payload)=> {
+
+    if (err) {
+      t.fail(err, err)
+    }
+    else {
+      t.ok(bot, 'got the bot')
+      console.log(Object.keys(bot), payload)
+    }
+
+    // does not exit!
+    bot.rtm.terminate()
+    bot.closeRTM()
+
+    t.end()
+  })
+})
+
