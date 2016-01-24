324beb71bba5bd2d5a96aa291eee171d0b88fad3
Rafael Cosman <rafaelcosman@gmail.com>
2016-01-24 04:33:59 +0000

    Fixes erronious comments

--- examples/intent-chatamize-bot.js
+++ examples/intent-chatamize-bot.js
@@ -24,27 +24,13 @@ This bot demonstrates use of the chatamize.com api.
 
   Find your bot inside Slack to send it a direct message.
 
-  Say: "Hello"
+  Say: "hi"
 
-  The bot will reply "Hello!"
-
-  Say: "Attach"
-
-  The bot will send a message with a multi-field attachment.
-
-  Send: "dm"
-
-  The bot will reply with a direct message.
-
-  Make sure to invite your bot into other channels using /invite @<my bot>!
+  The bot will reply with one of the greetings that you set on chatamize.com
 
 # EXTEND THE BOT:
 
-  Botkit is has many features for building cool and useful bots!
-
-  Read all about it here:
-
-    -> http://howdy.ai/botkit
+  Add more intents and messages to chatamize.com and you can use them here!
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
