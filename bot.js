930fa7ea7b50036fbf4236ec72646902dcd1819e
Steef Min <steefmin@gmail.com>
2016-03-18 13:24:30 +0100

    moved non-convo functions to seperate file

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 require('./env.js');
+require('./functions.js');
 if (!process.env.TOKEN) {
 	console.log('Error: Specify token in environment');
     process.exit(1);
@@ -123,24 +124,6 @@ controller.hears(['ken ik jou','wie ben jij','hoe lang ben je al wakker','uptime
 	var uptime = formatUptime(process.uptime());
 	bot.reply(message,':robot_face: Ik ben een bot genaamd <@' + bot.identity.name + '>. Ik draai al  ' + uptime + ' op  ' + hostname + '.');
 });
-
-function formatUptime(uptime) {
-	var unit = 'seconden';
-	if (uptime > 60) {
-		uptime = uptime / 60;
-		unit = 'minuten';
-	}
-	if (uptime > 60) {
-		uptime = uptime / 60;
-		unit = 'uren';
-	}
-	if (uptime > 24) {
-	    uptime = uptime / 24;
-	    unit = 'dagen';
-	}
-	uptime = Math.round(uptime) + ' ' + unit;
-	return uptime;
-}
 
 controller.hears(['takenlijst','lijst'],'mention,direct_mention,ambient,direct_message',function(bot,message){
     if(message.event=="direct_message"){
@@ -388,31 +371,4 @@ UpdateDeadline = function(response,convo){
 			bot.reply(response,"Ok, nieuwe deadline genoteerd.");
 		}
 	});
-}
-
-function verifyDate(text) {
-	if (text == "vandaag" || text == "Vandaag"){
-		var date = new Date();
-	}else if(text == "morgen" || text == "Morgen"){
-		var date = new Date();
-		date.setDate(date.getDate() + 1);
-	}else{
-		text = text.replace(/-/g,"/");
-		var split = text.split('/');
-		if(typeof split[1] != "undefined" && typeof split[2] != "undefined"){
-			text = split[1]+'/'+split[0]+'/'+split[2];
-		}
-		text = text.replace("maa","mar");
-		text = text.replace("mei","may");
-		text = text.replace("okt","oct");
-		var date = new Date(Date.parse(text));
-		date.setDate(date.getDate() + 1);
-	}
-	var current_date = new Date();
-	current_date= new Date(Date.parse(current_date.toDateString()));
-	if(date != "Invalid Date" && date.getTime()>=current_date.getTime()){
-		return false;
-	}else{
-		return date;
-	}
 }
---
+++ functions.js
@@ -0,0 +1,44 @@
+function formatUptime(uptime) {
+	var unit = 'seconden';
+	if (uptime > 60) {
+		uptime = uptime / 60;
+		unit = 'minuten';
+	}
+	if (uptime > 60) {
+		uptime = uptime / 60;
+		unit = 'uren';
+	}
+	if (uptime > 24) {
+	    uptime = uptime / 24;
+	    unit = 'dagen';
+	}
+	uptime = Math.round(uptime) + ' ' + unit;
+	return uptime;
+}
+
+function verifyDate(text) {
+	if (text == "vandaag" || text == "Vandaag"){
+		var date = new Date();
+	}else if(text == "morgen" || text == "Morgen"){
+		var date = new Date();
+		date.setDate(date.getDate() + 1);
+	}else{
+		text = text.replace(/-/g,"/");
+		var split = text.split('/');
+		if(typeof split[1] != "undefined" && typeof split[2] != "undefined"){
+			text = split[1]+'/'+split[0]+'/'+split[2];
+		}
+		text = text.replace("maa","mar");
+		text = text.replace("mei","may");
+		text = text.replace("okt","oct");
+		var date = new Date(Date.parse(text));
+		date.setDate(date.getDate() + 1);
+	}
+	var current_date = new Date();
+	current_date= new Date(Date.parse(current_date.toDateString()));
+	if(date != "Invalid Date" && date.getTime()>=current_date.getTime()){
+		return false;
+	}else{
+		return date;
+	}
+}
