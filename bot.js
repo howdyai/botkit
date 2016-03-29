6a6a48dabd7df46e7ca5d381c7cdfc98fa05d562
Ben Brown <ben@xoxco.com>
2016-03-29 17:40:46 -0500

    Enable developer to change pattern matching function at a global level Use the same global pattern matching function inside convo.ask as is used at controller.hears Strip @mentions from beginning of messages that are part of a conversation Add additional .ask examples in bot.js

--- bot.js
+++ bot.js
@@ -103,9 +103,8 @@ controller.hears(['hello','hi'],'direct_message,direct_mention,mention',function
     });
 });
 
-controller.hears(['call me (.*)'],'direct_message,direct_mention,mention',function(bot, message) {
-    var matches = message.text.match(/call me (.*)/i);
-    var name = matches[1];
+controller.hears(['call me (.*)','my name is (.*)'],'direct_message,direct_mention,mention',function(bot, message) {
+    var name = message.match[1];
     controller.storage.users.get(message.user,function(err, user) {
         if (!user) {
             user = {
@@ -125,7 +124,64 @@ controller.hears(['what is my name','who am i'],'direct_message,direct_mention,m
         if (user && user.name) {
             bot.reply(message,'Your name is ' + user.name);
         } else {
-            bot.reply(message,'I don\'t know yet!');
+            bot.startConversation(message, function(err, convo) {
+                if (!err) {
+                    convo.say('I do not know your name yet!');
+                    convo.ask('What should I call you?', function(response, convo) {
+                        convo.ask('You want me to call you `' + response.text + '`?', [
+                            {
+                                pattern: 'yes',
+                                callback: function(response, convo) {
+                                    // since no further messages are queued after this,
+                                    // the conversation will end naturally with status == 'completed'
+                                    convo.next();
+                                }
+                            },
+                            {
+                                pattern: 'no',
+                                callback: function(response, convo) {
+                                    // stop the conversation. this will cause it to end with status == 'stopped'
+                                    convo.stop();
+                                }
+                            },
+                            {
+                                default: true,
+                                callback: function(response, convo) {
+                                    convo.repeat();
+                                    convo.next();
+                                }
+                            }
+                        ]);
+
+                        convo.next();
+
+                    },{'key': 'nickname'}); // store the results in a field called nickname
+
+                    convo.on('end', function(convo) {
+                        if (convo.status == 'completed') {
+                            bot.reply(message,'OK! I will update my dossier...');
+
+                            controller.storage.users.get(message.user,function(err, user) {
+                                if (!user) {
+                                    user = {
+                                        id: message.user,
+                                    };
+                                }
+                                user.name = convo.extractResponse('nickname');
+                                controller.storage.users.save(user,function(err, id) {
+                                    bot.reply(message,'Got it. I will call you ' + user.name + ' from now on.');
+                                });
+                            });
+
+
+
+                        } else {
+                            // this happens if the conversation ended prematurely for some reason
+                            bot.reply(message, 'OK, nevermind!');
+                        }
+                    });
+                }
+            });
         }
     });
 });
