14f93bf599bdff3c7cf62a6a8f6fe19600a32d7d
Ben Brown <ben@xoxco.com>
2016-03-31 14:56:55 -0500

    Add documentation for controller.changeEars()

--- bot.js
+++ bot.js
@@ -81,48 +81,48 @@ var bot = controller.spawn({
 }).startRTM();
 
 
-controller.hears(['hello','hi'],'direct_message,direct_mention,mention',function(bot, message) {
+controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
 
     bot.api.reactions.add({
         timestamp: message.ts,
         channel: message.channel,
         name: 'robot_face',
-    },function(err, res) {
+    }, function(err, res) {
         if (err) {
-            bot.botkit.log('Failed to add emoji reaction :(',err);
+            bot.botkit.log('Failed to add emoji reaction :(', err);
         }
     });
 
 
-    controller.storage.users.get(message.user,function(err, user) {
+    controller.storage.users.get(message.user, function(err, user) {
         if (user && user.name) {
-            bot.reply(message,'Hello ' + user.name + '!!');
+            bot.reply(message, 'Hello ' + user.name + '!!');
         } else {
-            bot.reply(message,'Hello.');
+            bot.reply(message, 'Hello.');
         }
     });
 });
 
-controller.hears(['call me (.*)','my name is (.*)'],'direct_message,direct_mention,mention',function(bot, message) {
+controller.hears(['call me (.*)', 'my name is (.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
     var name = message.match[1];
-    controller.storage.users.get(message.user,function(err, user) {
+    controller.storage.users.get(message.user, function(err, user) {
         if (!user) {
             user = {
                 id: message.user,
             };
         }
         user.name = name;
-        controller.storage.users.save(user,function(err, id) {
-            bot.reply(message,'Got it. I will call you ' + user.name + ' from now on.');
+        controller.storage.users.save(user, function(err, id) {
+            bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
         });
     });
 });
 
-controller.hears(['what is my name','who am i'],'direct_message,direct_mention,mention',function(bot, message) {
+controller.hears(['what is my name', 'who am i'], 'direct_message,direct_mention,mention', function(bot, message) {
 
-    controller.storage.users.get(message.user,function(err, user) {
+    controller.storage.users.get(message.user, function(err, user) {
         if (user && user.name) {
-            bot.reply(message,'Your name is ' + user.name);
+            bot.reply(message, 'Your name is ' + user.name);
         } else {
             bot.startConversation(message, function(err, convo) {
                 if (!err) {
@@ -155,21 +155,21 @@ controller.hears(['what is my name','who am i'],'direct_message,direct_mention,m
 
                         convo.next();
 
-                    },{'key': 'nickname'}); // store the results in a field called nickname
+                    }, {'key': 'nickname'}); // store the results in a field called nickname
 
                     convo.on('end', function(convo) {
                         if (convo.status == 'completed') {
-                            bot.reply(message,'OK! I will update my dossier...');
+                            bot.reply(message, 'OK! I will update my dossier...');
 
-                            controller.storage.users.get(message.user,function(err, user) {
+                            controller.storage.users.get(message.user, function(err, user) {
                                 if (!user) {
                                     user = {
                                         id: message.user,
                                     };
                                 }
                                 user.name = convo.extractResponse('nickname');
-                                controller.storage.users.save(user,function(err, id) {
-                                    bot.reply(message,'Got it. I will call you ' + user.name + ' from now on.');
+                                controller.storage.users.save(user, function(err, id) {
+                                    bot.reply(message, 'Got it. I will call you ' + user.name + ' from now on.');
                                 });
                             });
 
@@ -187,11 +187,11 @@ controller.hears(['what is my name','who am i'],'direct_message,direct_mention,m
 });
 
 
-controller.hears(['shutdown'],'direct_message,direct_mention,mention',function(bot, message) {
+controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
 
-    bot.startConversation(message,function(err, convo) {
+    bot.startConversation(message, function(err, convo) {
 
-        convo.ask('Are you sure you want me to shutdown?',[
+        convo.ask('Are you sure you want me to shutdown?', [
             {
                 pattern: bot.utterances.yes,
                 callback: function(response, convo) {
@@ -199,7 +199,7 @@ controller.hears(['shutdown'],'direct_message,direct_mention,mention',function(b
                     convo.next();
                     setTimeout(function() {
                         process.exit();
-                    },3000);
+                    }, 3000);
                 }
             },
         {
@@ -215,14 +215,17 @@ controller.hears(['shutdown'],'direct_message,direct_mention,mention',function(b
 });
 
 
-controller.hears(['uptime','identify yourself','who are you','what is your name'],'direct_message,direct_mention,mention',function(bot, message) {
+controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
+    'direct_message,direct_mention,mention', function(bot, message) {
 
-    var hostname = os.hostname();
-    var uptime = formatUptime(process.uptime());
+        var hostname = os.hostname();
+        var uptime = formatUptime(process.uptime());
 
-    bot.reply(message,':robot_face: I am a bot named <@' + bot.identity.name + '>. I have been running for ' + uptime + ' on ' + hostname + '.');
+        bot.reply(message,
+            ':robot_face: I am a bot named <@' + bot.identity.name +
+             '>. I have been running for ' + uptime + ' on ' + hostname + '.');
 
-});
+    });
 
 function formatUptime(uptime) {
     var unit = 'second';
