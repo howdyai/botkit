d19aaf55c8787db8b87a1174475d5bd97ec881ec
rocketsummer <rocketsummer@users.noreply.github.com>
2016-05-19 22:20:15 -0500

    Add CLI options to facebook bot: --lt to get a web URL and --ltsubdomain to set the subdomain. Update instructions in facebook readme with new CLI options. Fix syntax error in storage_test.js, caught by npm run test.

--- facebook_bot.js
+++ facebook_bot.js
@@ -25,11 +25,9 @@ This bot demonstrates many of the core features of Botkit:
 
   Run your bot from the command line:
 
-    page_token=<MY PAGE TOKEN> verify_token=<MY_VERIFY_TOKEN> node facebook_bot.js
+    page_token=<MY PAGE TOKEN> verify_token=<MY_VERIFY_TOKEN> node facebook_bot.js [--lt [--ltsubdomain LOCALTUNNEL_SUBDOMAIN]]
 
-  Use localtunnel.me to make your bot available on the web:
-
-    lt --port 3000
+  Use the --lt option to make your bot available on the web through localtunnel.me.
 
 # USE THE BOT:
 
@@ -78,9 +76,24 @@ if (!process.env.verify_token) {
     process.exit(1);
 }
 
-
 var Botkit = require('./lib/Botkit.js');
 var os = require('os');
+var commandLineArgs = require('command-line-args');
+var localtunnel = require('localtunnel');
+
+const cli = commandLineArgs([
+      {name: 'lt', alias: 'l', args: 1, description: 'Use localtunnel.me to make your bot available on the web.',
+      type: Boolean, defaultValue: false},
+      {name: 'ltsubdomain', alias: 's', args: 1,
+      description: 'Custom subdomain for the localtunnel.me URL. This option can only be used together with --lt.',
+      type: String, defaultValue: null},
+   ]);
+
+const ops = cli.parse();
+if(ops.lt === false && ops.ltsubdomain !== null) {
+    console.log("error: --ltsubdomain can only be used together with --lt.");
+    process.exit();
+}
 
 var controller = Botkit.facebookbot({
     debug: true,
@@ -94,13 +107,25 @@ var bot = controller.spawn({
 controller.setupWebserver(process.env.port || 3000, function(err, webserver) {
     controller.createWebhookEndpoints(webserver, bot, function() {
         console.log('ONLINE!');
+        if(ops.lt) {
+            var tunnel = localtunnel(process.env.port || 3000, {subdomain: ops.ltsubdomain}, function(err, tunnel) {
+                if (err) {
+                    console.log(err);
+                    process.exit();
+                }
+                console.log("Your bot is available on the web at the following URL: " + tunnel.url + '/facebook/receive');
+            });
+
+            tunnel.on('close', function() {
+                console.log("Your bot is no longer available on the web at the localtunnnel.me URL.");
+                process.exit();
+            });
+        }
     });
 });
 
 
 controller.hears(['hello', 'hi'], 'message_received', function(bot, message) {
-
-
     controller.storage.users.get(message.user, function(err, user) {
         if (user && user.name) {
             bot.reply(message, 'Hello ' + user.name + '!!');
