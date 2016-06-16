caffb3cfc0b1b4cd2fe30ed2be64cf4b62b3efb0
Ben Brown <ben@xoxco.com>
2016-06-16 13:50:15 -0500

    updates

--- pro_bot.js
+++ pro_bot.js
@@ -78,9 +78,21 @@ var controller = Botkit.slackbot({
 
 var bot = controller.spawn({
     token: process.env.token,
-    BOTKIT_API_KEY:  process.env.botkit,
+    howdy_key: process.env.key,
+    howdy_secret: process.env.secret
 }).startRTM();
 
+// controller.loginToHowdy(bot,{
+//     username: 'John',
+//     password: 'robco',
+// }).then(function(session) {
+//
+//     console.log('GOT A SESSION', session);
+//
+// }).catch(function(err) {
+//     console.log('ERROR LOGGING IN ', err);
+//     throw new Error(err);
+// });
 
 controller.on('direct_message,direct_mention,mention', function(bot, message) {
     controller.triggerConversation(bot,message).then(function(convo) {
