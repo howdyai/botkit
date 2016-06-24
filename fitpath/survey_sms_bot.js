1909a84653b5a48e3e31d979993a6791238706a5
Shane Pelletier <lotios611@gmail.com>
2016-06-24 12:41:11 -0300

    survey_sms_bot.js

--- fitpath/survey_sms_bot.js
+++ fitpath/survey_sms_bot.js
@@ -1,16 +1,41 @@
 var Botkit = require('../lib/Botkit.js');
 
-var ConvoObject = {
-  userId: '5754345drt4654353',
-  userMedium: 'sms',
-  userContactInfo: {name: 'Josh', phoneNumber: '+15064706220'},
-  questions: [
-    'Do you like pancakes?',
-    'Do you like waffles?',
-    'Do you like french toast?'
-  ],
-  surveyId: '35435436afaf'
-};
+var ConvoObjects = [
+  {
+    userId: '5754345drt4654353',
+    userMedium: 'sms',
+    userContactInfo: {name: 'Josh', phoneNumber: '+15064261732'},
+    questions: [
+      'Do you like pancakes?',
+      'Do you like waffles?',
+      'Do you like french toast?'
+    ],
+    surveyId: '35435436afaf'
+  },
+  {
+    userId: '3456ytrafda',
+    userMedium: 'sms',
+    userContactInfo: {name: 'Bob', phoneNumber: '+15064261732'},
+    questions: [
+      'Do you like pancakes?',
+      'Do you like waffles?',
+      'Do you like french toast?'
+    ],
+    surveyId: '32432534642'
+  },
+  {
+    userId: '34q4wq4',
+    userMedium: 'sms',
+    userContactInfo: {name: 'George', phoneNumber: '+15064706220'},
+    questions: [
+      'Do you like pancakes?',
+      'Do you like waffles?',
+      'Do you like french toast?'
+    ],
+    surveyId: '23432423'
+  }
+];
+
 
 var controller = Botkit.twiliosmsbot({
   account_sid: 'ACf83693e222a7ade08080159c4871c9e3',
@@ -35,20 +60,43 @@ controller.setupWebserver(3000, function (err, webserver) {
 //     });
 // });
 
-// Currently broken
-/*controller.hears('.*', 'message_received', function (bot, message) {
+controller.hears('.*', 'message_received', function (bot, message) {
   bot.reply(message, 'Sorry, my programmer was too lazy to come up with a response for that.');
 });
-*/
 
-module.exports = function receiveConvo(convoObject) {
+
+module.exports.receiveConvo = function (convoObject) {
   console.log(JSON.stringify(convoObject));
-  bot.startConversation({channel: convoObject.userContactInfo.phoneNumber, user: convoObject.userContactInfo.phoneNumber, text: ''}, function (err, convo) {
+  bot.startPrivateConversation({user: convoObject.userContactInfo.phoneNumber, text: ''}, function (err, convo) {
+    convo.say('Hi! Here\'s a survey your coach wanted me to send you.');
     for (var i = 0; i < convoObject.questions.length; i++) {
       convo.ask(convoObject.questions[i], function (res, convo) {
         console.log(res.text);
         convo.next();
       });
     }
+    convo.say('Thanks for answering my questions. Enjoy the rest of your day ' + String.fromCodePoint(128578));
+    convo.on('end', function (convo) {
+      if (convo.status == 'completed') {
+        var responses = convo.extractResponses();
+
+        var responseArray = [];
+        // In order to send responses back as an array in the right order, loop through questions array
+        for (var i = 0; i < convoObject.questions.length; i++) {
+          var question = convoObject.questions[i];
+          // Responses are indexed by the question as a key
+          var response = responses[question];
+          // Push the response onto the responseArray
+          responseArray.push(response);
+        }
+        console.log(convo);
+        console.log(responseArray);
+      }
+    })
   });
 };
+
+for (var i = 0; i < ConvoObjects.length; i++) {
+  var ConvoObject = ConvoObjects[i];
+  module.exports.receiveConvo(ConvoObject);
+}
