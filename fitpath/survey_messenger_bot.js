2c9ba62e33d3551e9365f9ec3922e41de0f51b65
Shane Pelletier <lotios611@gmail.com>
2016-07-15 12:24:46 -0300

    Added ability for user to set their own reminders to the Messenger bot; integrated surveys + reminders with automated responses.

--- fitpath/survey_messenger_bot.js
+++ fitpath/survey_messenger_bot.js
@@ -1,4 +1,62 @@
 var Botkit = require('../lib/Botkit.js');
+var mongoose = require('mongoose');
+var RiveScript = require('rivescript');
+var schedule = require('node-schedule');
+
+function parseTime(timeStr, dt) {
+    if (!dt) {
+        dt = new Date();
+    }
+
+    var time = timeStr.match(/(\d+)(?::(\d\d))?\s*(p?)/i);
+    if (!time) {
+        return NaN;
+    }
+    var hours = parseInt(time[1], 10);
+    if (hours == 12 && !time[3]) {
+        hours = 0;
+    }
+    else {
+        hours += (hours < 12 && time[3]) ? 12 : 0;
+    }
+
+    dt.setHours(hours);
+    dt.setMinutes(parseInt(time[2], 10) || 0);
+    dt.setSeconds(0, 0);
+    return dt;
+}
+
+mongoose.connect('mongodb://shane:letmein1@localhost/development');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Connected to db');
+});
+
+var messageSchema = mongoose.Schema({
+  sentBy: String,
+  sentTo: String,
+  text: String
+});
+
+var Message = mongoose.model('Message', messageSchema);
+
+var reminderSchema = mongoose.Schema({
+  hour: Number,
+  minute: Number,
+  text: String,
+  originalMessage: Object
+});
+
+var Reminder = mongoose.model('Reminder', reminderSchema);
+
+var rivescript = new RiveScript();
+
+var controller = Botkit.facebookbot({
+  access_token: 'EAADXmpOGmZBQBAEtfcPjsGZCo5kYkptvjjZBOF3jcFPOEggsw25p6FRZBLyc5tZAlWQpj6C6fnR0nbSEzLgdCHxrQ8M57AN7pS8NzcR1ZAAwdzZBFD72lLPhRFOxB1ZAARZBrAZANBtQDNKejd9OtjjRBgkPtP7ZCUZBN4ywfEZAxuCRlogZDZD',
+  verify_token: 'FISHTACOS',
+});
 
 // For testing purposes; this should be stored on the FitPath side of things and passed in
 var authMessage;
@@ -48,7 +106,12 @@ var bot = controller.spawn({});
 
 controller.setupWebserver(27182, function (err, webserver) {
   controller.createWebhookEndpoints(controller.webserver, bot, function () {
-    console.log('TwilioMessengerBot is online!');
+    console.log('Loading brain...');
+    rivescript.loadDirectory(__dirname + '/brain', function () {
+      console.log('FitBot Messenger is online!');
+    }, function (error) {
+      console.log('Error when loading files: ' + error);
+    });
   });
 });
 
@@ -57,14 +120,56 @@ controller.on('facebook_optin', function (bot, message) {
   console.log(authMessage);
 });
 
-controller.hears(['hi', 'hey', 'hello', 'heya', 'ello', 'howdy'], 'message_received', function (bot, message) {
+controller.hears('survey', 'message_received', function (bot, message) {
   bot.reply(message, 'Hi! I\'ll be sending a survey to you shortly.');
   authMessage = message;
   console.log(message);
 });
 
 controller.hears('remind me to (.*) at (.*)', 'message_received', function (bot, message) {
-  bot.reply(message, 'Sure, I\'ll remind you to ' + message.match[1].replace('me', 'you') +  ' at ' + message.match[2]);
+  bot.reply(message, 'Sure, I\'ll remind you to ' + message.match[1].replace('me', 'you').replace('my', 'your') +  ' at ' + message.match[2]) + '.';
+  console.log(message.match[2]);
+  console.log(parseTime(message.match[2]));
+  Reminder.create({
+    hour: parseTime(message.match[2]).getHours(),
+    minute: parseTime(message.match[2]).getMinutes(),
+    text: message.match[1].replace('me', 'you').replace('my', 'your'),
+    originalMessage: message
+  }, function (err, reminder) {
+    if (err) console.log(err)
+    console.log(reminder);
+  });
+});
+
+controller.on('message_received', function (bot, message) {
+  // Create message document representing the message that the user sent the bot
+  // TODO: use actual values for sentTo and sentBy
+  // TODO: add error checking
+  Message.create({
+    sentTo: 'Bot',
+    sentBy: message.user,
+    text: message.text
+  });
+
+  console.log(message.text);
+
+  // If the other actions wouldn't be triggered
+  if (!message.text.includes('survey')    &&
+      message.text.search(/remind me to (.*) at (.*)/) === -1) {
+    console.log(message.text);
+    rivescript.sortReplies();
+    var reply = rivescript.reply('local-user', message.text);
+    console.log(reply);
+    bot.reply(message, reply);
+
+    // Create message document representing the message that the bot sent the user
+    // TODO: same as above
+    Message.create({
+      sentTo: message.user,
+      sentBy: 'Bot',
+      text: reply
+    });
+  }
 });
 
 module.exports.receiveConvo = function (convoObject) {
@@ -102,9 +207,29 @@ module.exports.receiveConvo = function (convoObject) {
 
 function trySendingSurvey () {
   if (typeof authMessage !== 'undefined') {
-    clearInterval(interval);
     module.exports.receiveConvo(ConvoObjects[0]);
+    authMessage = undefined;
   }
 }
 
 var interval = setInterval(trySendingSurvey, 10000);
+
+function trySendingReminders () {
+  console.log(new Date().getHours());
+  console.log(new Date().getMinutes());
+  console.log('Trying to send reminders...')
+  Reminder.find({hour: new Date().getHours(), minute: new Date().getMinutes()}, function (err, reminders) {
+    for (var i = 0; i < reminders.length; i++) {
+      console.log(reminders[i]);
+      var message = reminders[i].originalMessage;
+      message.text = 'Hey! You wanted me to remind you to ' + reminders[i].text + '.';
+      bot.say(message);
+    }
+  });
+}
+
+var rule = new schedule.RecurrenceRule();
+rule.dayOfWeek = [new schedule.Range(0, 6)];
+schedule.scheduleJob(rule, function () {
+  trySendingReminders();
+});
