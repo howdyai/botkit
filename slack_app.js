61d1e2306506296d4860daf5093a22d2b2f9824c
Dave Wong <dave@soapboxhq.com>
2016-10-21 12:04:11 -0400

    initial code for soapbox module

--- slack_app.js
+++ slack_app.js
@@ -1,6 +1,7 @@
 var Botkit = require('./lib/Botkit.js');
 var os = require('os');
 var config = require('getconfig');
+var Soapbox = require('./lib/soapbox/Soapbox.js');
 
 var controller = Botkit.slackbot({
     debug: config.botkit_debug,
@@ -8,9 +9,9 @@ var controller = Botkit.slackbot({
     json_file_store: config.botkit_json_file_store,
     redirectUri: config.slack_redirect_uri,
 }).configureSlackApp({
-  clientId: config.slack_client_id,
-  clientSecret: config.slack_client_secret,
-  scopes: config.slack_app_scopes
+    clientId: config.slack_client_id,
+    clientSecret: config.slack_client_secret,
+    scopes: config.slack_app_scopes
 });
 
 controller.setupSecureWebserver(config.port,function(err,webserver) {
@@ -33,3 +34,69 @@ var _bots = {};
 function trackBot(bot) {
   _bots[bot.config.token] = bot;
 }
+
+controller.on('create_bot',function(bot,config) {
+
+  if (_bots[bot.config.token]) {
+    // already online! do nothing.
+  } else {
+    bot.startRTM(function(err) {
+
+      if (!err) {
+        trackBot(bot);
+      }
+
+      bot.startPrivateConversation({user: config.createdBy},function(err,convo) {
+        if (err) {
+          console.log(err);
+        } else {
+          convo.say('I am a bot that has just joined your team');
+          convo.say('You must now /invite me to a channel so that I can be of use!');
+        }
+      });
+
+    });
+  }
+
+});
+
+// Handle events related to the websocket connection to Slack
+controller.on('rtm_open',function(bot) {
+  console.log('** The RTM api just connected!');
+});
+
+controller.on('rtm_close',function(bot) {
+  console.log('** The RTM api just closed');
+  bot.startRTM(function(err) {
+    if (err) {
+      console.log('***FAILED TO RESTART RTM');
+      exit(1);
+    }
+  })
+});
+
+controller.storage.teams.all(function(err,teams) {
+
+  if (err) {
+    throw new Error(err);
+  }
+
+  // connect all teams with bots up to slack!
+  for (var t  in teams) {
+    if (teams[t].bot) {
+      controller.spawn(teams[t]).startRTM(function(err, bot) {
+        if (err) {
+          console.log('Error connecting bot to Slack:',err);
+        } else {
+          trackBot(bot);
+        }
+      });
+    }
+  }
+});
+
+var soapbox = new Soapbox(controller);
+
+controller.hears('^create team$', ['direct_mention'], function(bot, message) {
+    soapbox.createTeam.create(bot, message);
+});
