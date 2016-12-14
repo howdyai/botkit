7a1ad869f257114fa9e8e8ea6febdc39d8bbe02c
Jon <me@jonchurch.com>
2016-12-14 16:59:28 -0500

    add validateRequest, abortOnValidationError

--- facebook_bot.js
+++ facebook_bot.js
@@ -99,6 +99,7 @@ var controller = Botkit.facebookbot({
     log: true,
     access_token: process.env.page_token,
     verify_token: process.env.verify_token,
+    validate_requests: true, // Refuse any requests that don't come from FB on your receive webhook, must provide FB_APP_SECRET in environment variables
 });
 
 var bot = controller.spawn({
