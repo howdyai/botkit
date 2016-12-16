7848c238421e6761651d71f97bdcb1796fc10a67
pkvenu@gmail.com <paw1983$10>
2016-12-15 19:33:00 -0800

    updated code to work with latest changes

--- glip_bot.js
+++ glip_bot.js
@@ -1,7 +1,8 @@
 /**
  * Created by pawan.venugopal on 10/31/16.
  */
-'use strict'
+
+"use strict";
 
 var Botkit = require('./lib/Botkit.js');
 var os = require('os');
@@ -13,12 +14,16 @@ var controller = Botkit.glipbot({
     debug: false,
 });
 
+
 var bot = controller.spawn({
-    host: process.env.HUBOT_GLIP_HOST || 'glip.com',
-    port: 443,
-    email: process.env.HUBOT_GLIP_EMAIL,
-    password: process.env.HUBOT_GLIP_PASSWORD,
-    identity: 'Botkit'
+    server: process.env.BOTKIT_GLIP_SERVER,
+    appKey: process.env.BOTKIT_GLIP_APPKEY,
+    appSecret: process.env.BOTKIT_GLIP_APPSECRET,
+    appName: 'GlipDemo',
+    appVersion: '1.0.0',
+    username: process.env.BOTKIT_GLIP_USERNAME,
+    password: process.env.BOTKIT_GLIP_PASSWORD,
+    extension: process.env.BOTKIT_GLIP_EXTENSION,
 }).startRTM();
 
 controller.setupWebserver(process.env.port || 3000, function(err, webserver){
@@ -30,74 +35,10 @@ controller.setupWebserver(process.env.port || 3000, function(err, webserver){
 
 });
 
-// controller.on('slash_command', function(bot, message) {
-//     bot.reply(message, 'This is a public reply to the ' + message.text + ' slash command!');
-//
-// });
-//
-// // reply to a direct mention - @bot hello
-// controller.on('direct_mention',function(bot,message) {
-//    // bot.reply(message, message.text);
-//     bot.startPrivateConversation(message, function(error, dm){
-//         dm.say(message.text);
-//     });
-// });
-//
-// controller.on('create_incoming_webhook', function(bot, webhook_config){
-//
-//     var options = {
-//         url: '/glip/receive'
-//     }
-//     bot.configureIncomingWebhook(options);
-//
-//     console.log('In send Webhook');
-//
-//     bot.sendWebhook({
-//         text: ':thumbsup: Incoming webhook sucessfully configured'
-//     })
-// });
-
-// Usage: weather SanFrancisco, CA
-controller.hears(["weather"],'message_received', function(bot, message){
-    var txt = message.text;
-    txt = txt.toLowerCase().replace('weather ','');
-    var city = txt.split(',')[0].trim().replace(' ','_');
-    var state = txt.split(',')[1].trim();
-    var key = 'Add your own key';
-
-    console.log(city + ', ' + state);
-    var url = '/api/' + key + '/forecast/q/state/city.json'
-    url = url.replace('state', state);
-    url = url.replace('city', city);
-
-    http.get({
-        host: 'api.wunderground.com',
-        path: url
-    }, function(response){
-        var body = '';
-        response.on('data',function(d){
-            body += d;
-        })
-        response.on('end', function(){
-            var data = JSON.parse(body);
-            console.log(data);
-            var days = data.forecast.simpleforecast.forecastday;
-             for( var i = 0; i<days.length; i++)
-            {
-                //console.log(days[i]);
-                bot.reply(message, days[i].date.weekday +
-                    ' high: ' + days[i].high.fahrenheit +
-                    ' low: ' + days[i].low.fahrenheit +
-                    ' condition: ' + days[i].conditions);
-                bot.reply(message, days[i].icon_url);
-             }
-        })
-    })
-});
 
 // Usage: uptime
 controller.hears(['uptime'],'message_received',function(bot, message) {
-    console.log('uptime in glipbot.js file: ' + JSON.stringify(message));
+    //console.log('uptime in glipbot.js file: ' + JSON.stringify(message));
 
     var hostname = os.hostname();
     var uptime = formatUptime(process.uptime());
@@ -153,30 +94,33 @@ controller.hears(['question me'], 'message_received', function(bot,message) {
 });
 
 //usage: pizzatime
-controller.hears(['pizzatime'], 'message_received', function(bot,message) {
-    var askFlavor = function(err, convo) {
-        convo.ask('What flavor of pizza do you want?', function(response, convo) {
-            convo.say('Awesome.');
-            askSize(response, convo);
-            convo.next();
-        });
-    };
-    var askSize = function(response, convo) {
-        convo.ask('What size do you want?', function(response, convo) {
-            convo.say('Ok.')
-            askWhereDeliver(response, convo);
-            convo.next();
-        });
-    };
-    var askWhereDeliver = function(response, convo) {
-        convo.ask('So where do you want it delivered?', function(response, convo) {
-            convo.say('Ok! Good bye.');
-            convo.next();
-        });
-    };
-
+controller.hears(['pizzatime'],'message_received',function(bot,message) {
     bot.startConversation(message, askFlavor);
 });
+
+var askFlavor = function(response, convo) {
+    convo.ask("What flavor of pizza do you want?", function(response, convo) {
+        convo.say("Awesome.");
+        askSize(response, convo);
+        convo.next();
+    });
+}
+var askSize = function(response, convo) {
+    convo.ask("What size do you want?", function(response, convo) {
+        convo.say("Ok.")
+        askWhereDeliver(response, convo);
+        convo.next();
+    });
+}
+var askWhereDeliver = function(response, convo) {
+    convo.ask("So where do you want it delivered?", function(response, convo) {
+        convo.say("Ok! Goodbye.");
+        convo.next();
+    });
+}
+
+
+
 
 function formatUptime(uptime) {
     var unit = 'second';
