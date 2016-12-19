28c7ed66273280d862a16d088ffb03046af19889
Ben Brown <ben@xoxco.com>
2016-12-19 17:06:46 -0600

    Fix to example bot

--- facebook_bot.js
+++ facebook_bot.js
@@ -164,7 +164,7 @@ controller.hears(['quick'], 'message_received', function(bot, message) {
 
 });
 
-controller.hears(['hello', 'hi'], 'message_received,facebook_postback', function(bot, message) {
+controller.hears(['^hello', '^hi'], 'message_received,facebook_postback', function(bot, message) {
     controller.storage.users.get(message.user, function(err, user) {
         if (user && user.name) {
             bot.reply(message, 'Hello ' + user.name + '!!');
