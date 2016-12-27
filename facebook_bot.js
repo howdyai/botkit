d8ba588199169acc0ddd7adfa32c5c001115ca56
Ouadie Lahdioui <ouadiesoft@gmail.com>
2016-12-27 22:58:49 +0100

    Add an example of how to remove a single domain from the whitelist

--- facebook_bot.js
+++ facebook_bot.js
@@ -152,6 +152,7 @@ controller.api.thread_settings.menu([
 ]);
 controller.api.thread_settings.domain_whitelist('https://localhost');
 controller.api.thread_settings.domain_whitelist(['https://127.0.0.1', 'https://0.0.0.0']);
+controller.api.thread_settings.delete_domain_whitelist('https://localhost');
 controller.api.thread_settings.get_domain_whitelist(function (err, data)  {
     console.log('****** Whitelisted domains :', data);
 });
