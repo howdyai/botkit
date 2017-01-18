e4f757abe64174945318cfa2b5c7f239c36843fa
Ben Brown <ben@xoxco.com>
2017-01-18 14:05:15 -0600

    Fix mentions Add limit_to_org limit_to_domain options

--- spark_bot.js
+++ spark_bot.js
@@ -35,6 +35,8 @@ var controller = Botkit.sparkbot({
     public_address: process.env.public_address,
     ciscospark_access_token: process.env.access_token,
     studio_token: process.env.studio_token,
+//    limit_to_domain: ['howdy.ai','cisco.com'],
+    limit_to_org: 'Y2lzY29zcGFyazovL3VzL09SR0FOSVpBVElPTi9jb25zdW1lcg',
 });
 
 
@@ -100,9 +102,10 @@ if (process.env.studio_token) {
 }
 
 controller.on('direct_mention', function(bot, message) {
-    bot.reply(message, 'You mentioned me.');
+    bot.reply(message, 'You mentioned me and said, "' + message.text + '"');
 });
 
 controller.on('direct_message', function(bot, message) {
-    bot.reply(message, 'I got your private message.');
+    bot.reply(message, 'I got your private message. You said, "' + message.text + '"');
+
 });
