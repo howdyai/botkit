23ba3eddda25d71e7d6e20c71006115a5d0ea6db
Cole Furfaro-Strode <colestrode@gmail.com>
2017-05-20 23:55:55 -0400

    moves botkit module mocks and adds clearAllMocks call

--- __test__/lib/Botkit.test.js
+++ __test__/lib/Botkit.test.js
@@ -1,14 +1,15 @@
 let botkit;
 
-beforeEach(() => {
-    jest.mock('../../lib/CoreBot', () => 'corebot');
-    jest.mock('../../lib/SlackBot', () => 'slackbot');
-    jest.mock('../../lib/Facebook', () => 'facebook');
-    jest.mock('../../lib/TwilioIPMBot', () => 'twilio');
-    jest.mock('../../lib/BotFramework', () => 'botframework');
-    jest.mock('../../lib/CiscoSparkbot', () => 'spark');
-    jest.mock('../../lib/ConsoleBot', () => 'console');
+jest.mock('../../lib/CoreBot', () => 'corebot');
+jest.mock('../../lib/SlackBot', () => 'slackbot');
+jest.mock('../../lib/Facebook', () => 'facebook');
+jest.mock('../../lib/TwilioIPMBot', () => 'twilio');
+jest.mock('../../lib/BotFramework', () => 'botframework');
+jest.mock('../../lib/CiscoSparkbot', () => 'spark');
+jest.mock('../../lib/ConsoleBot', () => 'console');
 
+beforeEach(() => {
+    jest.clearAllMocks();
     botkit = require('../../lib/Botkit');
 });
 
