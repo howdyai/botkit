01d109d7f901ab364d7558123af706f42184c1b3
Ben Brown <ben@xoxco.com>
2017-06-28 16:34:07 -0500

    ability for bot to start a new convresation in the channel

--- teamsbot.js
+++ teamsbot.js
@@ -12,9 +12,58 @@ var controller = Botkit.teamsbot({
 controller.on('invoke', function(bot, message) {
 
   console.log('INVOKE BUTTON CLICKED');
+  if (message.name == 'composeExtension/query') {
+      console.log('COMPOSE EXTENSION!');
 
-  bot.reply(message, 'heard an invoke postback')
-
+      message.http_res.send({
+  "composeExtension":{
+    "type":"result",
+    "channelData":{
+    },
+    "attachmentLayout":"list",
+    attachments: [
+      {
+        contentType: 'application/vnd.microsoft.card.hero',
+        content: {
+          title: 'Hero card',
+          subtitle: 'Subtitle of hero card',
+          text: 'This is the text',
+          images: [
+            {
+                url: 'http://placekitten.com/1600/900',
+                alt: 'a kitten',
+                tap: {
+                  type: 'invoke',
+                  title: 'picture click',
+                  value: JSON.stringify({'foo':'bar'}),
+                }
+            }
+          ],
+          buttons: [
+            {
+              title: 'Drink me',
+              type: 'invoke',
+              value: JSON.stringify({'foo':'bar'}),
+            },
+            {
+              title: 'Drink me',
+              type: 'imBack',
+              value: 'I CLICK A BUTTON',
+            },
+            {
+              title: 'Open Url',
+              type: 'openUrl',
+              value: 'https://botkit.ai',
+            },
+          ]
+        }
+      }
+    ]
+  }
+});
+  } else {
+    bot.reply(message, 'heard an invoke postback')
+  }
 })
 
 controller.on('mention', function(bot, message) {
@@ -23,6 +72,11 @@ controller.on('mention', function(bot, message) {
 
 
 });
+
+controller.hears('thread','direct_mention', function(bot, message) {
+  bot.replyInThread(message,'New thread', function(err) {});
+});
+
 controller.hears('.*','direct_message, direct_mention', function(bot, message) {
 
   bot.reply(message, {
