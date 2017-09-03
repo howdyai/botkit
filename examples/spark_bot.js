b31eb11bc0bc34c7f0433482ab363c36af0d44a6
pathiyani <pathiyani@gmail.com>
2017-09-03 23:32:08 +0700

    Update spark_bot.js
    I'm Noober

--- examples/spark_bot.js
+++ examples/spark_bot.js
@@ -88,7 +88,7 @@ if (process.env.studio_token) {
     controller.on('direct_message,direct_mention', function(bot, message) {
         controller.studio.runTrigger(bot, message.text, message.user, message.channel).then(function(convo) {
             if (!convo) {
-                // console.log('NO STUDIO MATCH');
+         console.log('NO STUDIO MATCH');
             } else {
               // found a conversation
             }
