58071a2755f75de054a18fc1be2f9df2c55f2c8e
Sean <fastbeanau@gmail.com>
2018-09-24 17:19:49 +1000

    Refactor simple storage unit tests

--- tests/unit/storage.test.js
+++ tests/unit/storage.test.js
@@ -44,237 +44,87 @@ const testObj1 = {id: 'TEST1', foo: 'bar1'};
 describe('Simple Storage', () => {
     const storage = require('../../lib/storage/simple_storage.js')();
 
-    describe('Has correct methods', () => {
+    Object.keys(storage).forEach((key) => {
+        const store = storage[key];
 
-        test('Channels interfaces', () => {
-            expect(storage.channels).toBeDefined();
-            expect(storage.channels.save).toBeInstanceOf(Function);
-            expect(storage.channels.get).toBeInstanceOf(Function);
-            expect(storage.channels.all).toBeInstanceOf(Function);
-            expect(storage.channels.delete).toBeInstanceOf(Function);
+        describe('Has correct methods', () => {
+
+            test(`Interfaces for ${key}`, () => {
+                expect(store).toBeDefined();
+                expect(store.save).toBeInstanceOf(Function);
+                expect(store.get).toBeInstanceOf(Function);
+                expect(store.all).toBeInstanceOf(Function);
+                expect(store.delete).toBeInstanceOf(Function);
+            });
         });
+    
+        describe(`Operations for ${key}`, () => {
+            const store = storage[key];
 
-        test('Teams interfaces', () => {
-            expect(storage.teams).toBeDefined();
-            expect(storage.teams.save).toBeInstanceOf(Function);
-            expect(storage.teams.get).toBeInstanceOf(Function);
-            expect(storage.teams.all).toBeInstanceOf(Function);
-            expect(storage.teams.delete).toBeInstanceOf(Function);
-        });
-
-        test('Users interfaces', () => {
-            expect(storage.users).toBeDefined();
-            expect(storage.users.save).toBeInstanceOf(Function);
-            expect(storage.users.get).toBeInstanceOf(Function);
-            expect(storage.users.all).toBeInstanceOf(Function);
-            expect(storage.users.delete).toBeInstanceOf(Function);
-        });
-
-    });
-
-    describe('Channels operations', () => {
-        const store = storage.channels;
-
-        test('Save', (done) => {
-            store.save(testObj0, (err) => {
-                expect(err).toBeNullOrUndefined();
-                store.save(testObj1, (err) => {
-                    expect(err).toBeNull();
-                    done();
+            test('Save', (done) => {
+                store.save(testObj0, (err) => {
+                    expect(err).toBeNullOrUndefined();
+                    store.save(testObj1, (err) => {
+                        expect(err).toBeNull();
+                        done();
+                    });
                 });
             });
-        });
 
-        test('Get', (done) => {
-            store.get(testObj0.id, (err, data) => {
-                expect(err).toBeNullOrUndefined();
-                expect(data).toEqual(testObj0);
-                done();
-            });
-        });
-
-        test('Get - data not present', (done) => {
-            store.get('TESTX', (err, data) => {
-                expect(err).toBeTruthy();
-                expect(data).toBeNullOrUndefined();
-                done();
-            });
-        });
-
-        test('All', (done) => {
-            store.all((err, data) => {
-                expect(err).toBeNullOrUndefined();
-                expect(data).toEqual([testObj0, testObj1]);
-                done();
-            });
-        });
-
-        test('Delete', (done) => {
-            store.delete(testObj1.id, (err) => {
-                expect(err).toBeNullOrUndefined();
-                store.all((err, data) => {
-                    expect(err).toBeNull();
-                    expect(data).toEqual([testObj0]);
-                    done();
-                });
-            });
-        });
-
-        test('Delete - data not present', (done) => {
-            store.get('TESTX', (err) => {
-                expect(err).toBeTruthy();
-                done();
-            });
-        });
-
-        test('Overwrite', (done) => {
-            const overwriteObject = {id: 'TEST0', foo: 'CHANGED'};
-            store.save(overwriteObject, (err) => {
-                expect(err).toBeNullOrUndefined();
+            test('Get', (done) => {
                 store.get(testObj0.id, (err, data) => {
                     expect(err).toBeNullOrUndefined();
-                    expect(data).toEqual(overwriteObject);
+                    expect(data).toEqual(testObj0);
                     done();
                 });
             });
-        });
 
-    });
-
-    describe('Teams operations', () => {
-        const store = storage.teams;
-
-        test('Save', (done) => {
-            store.save(testObj0, (err) => {
-                expect(err).toBeNullOrUndefined();
-                store.save(testObj1, (err) => {
-                    expect(err).toBeNull();
+            test('Get - data not present', (done) => {
+                store.get('TESTX', (err, data) => {
+                    expect(err).toBeTruthy();
+                    expect(data).toBeNullOrUndefined();
                     done();
                 });
             });
-        });
 
-        test('Get', (done) => {
-            store.get(testObj0.id, (err, data) => {
-                expect(err).toBeNullOrUndefined();
-                expect(data).toEqual(testObj0);
-                done();
-            });
-        });
-
-        test('Get - data not present', (done) => {
-            store.get('TESTX', (err, data) => {
-                expect(err).toBeTruthy();
-                expect(data).toBeNullOrUndefined();
-                done();
-            });
-        });
-
-        test('All', (done) => {
-            store.all((err, data) => {
-                expect(err).toBeNullOrUndefined();
-                expect(data).toEqual([testObj0, testObj1]);
-                done();
-            });
-        });
-
-        test('Delete', (done) => {
-            store.delete(testObj1.id, (err) => {
-                expect(err).toBeNullOrUndefined();
+            test('All', (done) => {
                 store.all((err, data) => {
-                    expect(err).toBeNull();
-                    expect(data).toEqual([testObj0]);
-                    done();
-                });
-            });
-        });
-
-        test('Delete - data not present', (done) => {
-            store.get('TESTX', (err) => {
-                expect(err).toBeTruthy();
-                done();
-            });
-        });
-
-        test('Overwrite', (done) => {
-            const overwriteObject = {id: 'TEST0', foo: 'CHANGED'};
-            store.save(overwriteObject, (err) => {
-                expect(err).toBeNullOrUndefined();
-                store.get(testObj0.id, (err, data) => {
                     expect(err).toBeNullOrUndefined();
-                    expect(data).toEqual(overwriteObject);
+                    expect(data).toEqual([testObj0, testObj1]);
                     done();
                 });
             });
-        });
 
-    });
-
-    describe('Users operations', () => {
-        const store = storage.users;
-
-        test('Save', (done) => {
-            store.save(testObj0, (err) => {
-                expect(err).toBeNullOrUndefined();
-                store.save(testObj1, (err) => {
-                    expect(err).toBeNull();
-                    done();
-                });
-            });
-        });
-
-        test('Get', (done) => {
-            store.get(testObj0.id, (err, data) => {
-                expect(err).toBeNullOrUndefined();
-                expect(data).toEqual(testObj0);
-                done();
-            });
-        });
-
-        test('Get - data not present', (done) => {
-            store.get('TESTX', (err, data) => {
-                expect(err).toBeTruthy();
-                expect(data).toBeNullOrUndefined();
-                done();
-            });
-        });
-
-        test('All', (done) => {
-            store.all((err, data) => {
-                expect(err).toBeNullOrUndefined();
-                expect(data).toEqual([testObj0, testObj1]);
-                done();
-            });
-        });
-
-        test('Delete', (done) => {
-            store.delete(testObj1.id, (err) => {
-                expect(err).toBeNullOrUndefined();
-                store.all((err, data) => {
-                    expect(err).toBeNull();
-                    expect(data).toEqual([testObj0]);
-                    done();
-                });
-            });
-        });
-
-        test('Delete - data not present', (done) => {
-            store.get('TESTX', (err) => {
-                expect(err).toBeTruthy();
-                done();
-            });
-        });
-
-        test('Overwrite', (done) => {
-            const overwriteObject = {id: 'TEST0', foo: 'CHANGED'};
-            store.save(overwriteObject, (err) => {
-                expect(err).toBeNullOrUndefined();
-                store.get(testObj0.id, (err, data) => {
+            test('Delete', (done) => {
+                store.delete(testObj1.id, (err) => {
                     expect(err).toBeNullOrUndefined();
-                    expect(data).toEqual(overwriteObject);
+                    store.all((err, data) => {
+                        expect(err).toBeNull();
+                        expect(data).toEqual([testObj0]);
+                        done();
+                    });
+                });
+            });
+
+            test('Delete - data not present', (done) => {
+                store.get('TESTX', (err) => {
+                    expect(err).toBeTruthy();
                     done();
                 });
             });
+
+            test('Overwrite', (done) => {
+                const overwriteObject = {id: 'TEST0', foo: 'CHANGED'};
+                store.save(overwriteObject, (err) => {
+                    expect(err).toBeNullOrUndefined();
+                    store.get(testObj0.id, (err, data) => {
+                        expect(err).toBeNullOrUndefined();
+                        expect(data).toEqual(overwriteObject);
+                        done();
+                    });
+                });
+            });
+
         });
 
     });
