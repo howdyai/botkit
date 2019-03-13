ae54888cc11639eacc8a9107d7e313e104e7f3c3
Ben Brown <benbro@microsoft.com>
2019-03-13 13:37:57 -0500

    scaffolding for modules

---
+++ packages/botbuilder-facebook/src/botworker.ts
@@ -0,0 +1,9 @@
+import { BotWorker, BotkitMessage } from 'botkit';
+
+export class FacebookWorker extends BotWorker {
+
+    constructor(botkit, config) {
+        super(botkit, config);
+    }
+
+}
