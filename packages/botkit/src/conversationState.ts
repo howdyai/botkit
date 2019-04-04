b9a122fd04c7ffd01b5420d664d36b6c3447a5e9
Ben Brown <benbro@microsoft.com>
2019-04-04 12:08:16 -0500

    flesh out docs

--- packages/botkit/src/conversationState.ts
+++ packages/botkit/src/conversationState.ts
@@ -3,15 +3,18 @@
  */
 import { Activity, ConversationState, Storage, TurnContext } from 'botbuilder';
 
+/**
+ * A customized version of [ConversationState](https://docs.microsoft.com/en-us/javascript/api/botbuilder-core/conversationstate?view=botbuilder-ts-latest) that  overide the [getStorageKey](#getStorageKey) method to create a more complex key value.
+ * This allows Botkit to automatically track conversation state in scenarios where multiple users are present in a single channel,
+ * or when threads or sub-channels parent channel that would normally collide based on the information defined in the conversation address field.
+ * Note: This is used automatically inside Botkit and developers should not need to directly interact with it.
+ */
 export class BotkitConversationState extends ConversationState {
 
     constructor(storage: Storage, namespace?: string) {
         super(storage, namespace);
     }
 
-    // overide the getStorageKey method to create a more complex value
-    // this gives Botkit better support for things like multiple users inside a single channel
-    // and threads or sub-channels that might exist in a parent channel
     public getStorageKey(context: TurnContext): string | undefined {
         const activity: Activity = context.activity;
         const channelId: string = activity.channelId;
--- packages/docs/reference/core.md
+++ packages/docs/reference/core.md
@@ -858,7 +858,10 @@ conversation.say('Let us get started...');
 
 <a name="BotkitConversationState"></a>
 ## BotkitConversationState
-
+A customized version of [ConversationState](https://docs.microsoft.com/en-us/javascript/api/botbuilder-core/conversationstate?view=botbuilder-ts-latest) that  overide the [getStorageKey](#getStorageKey) method to create a more complex key value.
+This allows Botkit to automatically track conversation state in scenarios where multiple users are present in a single channel,
+or when threads or sub-channels parent channel that would normally collide based on the information defined in the conversation address field.
+Note: This is used automatically inside Botkit and developers should not need to directly interact with it.
 ### constructor new BotkitConversationState()
 
 
