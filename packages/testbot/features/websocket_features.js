fc2a717ed982733deb80131ea76c3695993a366d
Ben Brown <benbro@microsoft.com>
2019-05-10 12:36:40 -0500

    cleanup testbot

--- packages/testbot/features/websocket_features.js
+++ packages/testbot/features/websocket_features.js
@@ -1,6 +1,8 @@
+const { BotkitConversation } = require('botkit');
+
 module.exports = function(controller) {
 
-    if (controller.adapter.name === 'Websocket Adapter') {
+    if (controller.adapter.name === 'Web Adapter') {
 
         console.log('Loading sample Websocket features...');
 
@@ -41,6 +43,31 @@ module.exports = function(controller) {
             });
         });
 
+        let typing = new BotkitConversation('typing', controller);
+
+        typing.say('I am going to type for a while now...');
+        typing.addAction('typing');
+    
+        // start the typing indicator
+        typing.addMessage({type: 'typing'}, 'typing');
+        // trigger a gotoThread, which gives us an opportunity to delay the next message
+        typing.addAction('next_thread','typing');
+    
+        typing.addMessage('typed!','next_thread');
+    
+       // use the before handler to delay the next message 
+        typing.before('next_thread',  async() => {
+            return new Promise((resolve, reject) => {
+                // simulate some long running process
+                setTimeout(resolve, 3000);
+            });
+        });
+    
+        controller.addDialog(typing);
+    
+        controller.hears('typing dialog', 'message', async(bot, message) => {
+            await bot.beginDialog('typing');
+        });
 
     }
 
