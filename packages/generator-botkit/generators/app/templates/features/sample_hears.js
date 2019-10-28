223724e6a2131d708be8ddf0b742ab55e8181818
Ben Brown <benbro@microsoft.com>
2019-10-28 16:36:37 -0500

    Fixes #1750 - update all environment variables to be uppercase

--- packages/generator-botkit/generators/app/templates/features/sample_hears.js
+++ packages/generator-botkit/generators/app/templates/features/sample_hears.js
@@ -5,7 +5,7 @@
 module.exports = function(controller) {
 
     // use a function to match a condition in the message
-    controller.hears(async(message) => message.text && message.text.toLowerCase() === 'foo', ['message'], async (bot, message) => {
+    controller.hears(async (message) => message.text && message.text.toLowerCase() === 'foo', ['message'], async (bot, message) => {
         await bot.reply(message, 'I heard "foo" via a function test');
     });
 
